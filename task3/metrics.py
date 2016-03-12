2130525c99be636eac190eab6e6d113f26816cf6
vks4git <vks_m@mail.ru>
2016-03-12 21:20:00 +0300

    Fixed bug when using ubyte scalars could cause overflow.

--- task3/metrics.py
+++ task3/metrics.py
@@ -6,19 +6,19 @@ __author__ = 'vks'
 def manhattan(x, y):
     ans = 0.0
     for i in range(len(x)):
-        ans += abs(x[i] - y[i])
+        ans += abs(float(x[i]) - float(y[i]))
     return ans
 
 
 def euclides(x, y):
     ans = 0.0
     for i in range(len(x)):
-        ans += (x[i] - y[i]) ** 2
+        ans += (float(x[i]) - float(y[i])) ** 2
     return math.sqrt(ans)
 
 
 def l_inf(x, y):
     ans = 0.0
     for i in range(len(x)):
-        ans = max(ans, abs(x[i] - y[i]))
+        ans = max(ans, abs(float(x[i]) - float(y[i])))
     return ans
